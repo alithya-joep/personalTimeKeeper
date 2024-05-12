22650696d3c0051b40c212c9b90a98c2f2eab29b
Joseph Piotrowski <dewetha@gmail.com>
2024-05-12 08:54:12 -0500

    updated list comments

--- cmd/list.go
+++ cmd/list.go
@@ -14,13 +14,8 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "time proects time",
+	Long:  `show the proects and there time for a week.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// get refrence to the projects
 		projects := myutils.Projects{}
